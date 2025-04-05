c8b1d94fd4b1ca6b46f1c61a9ce04c1d57b67c06
Bobbili Rajashekar Reddy <bobbilirajashekarreddy@Bobbilis-MacBook-Pro.local>
2025-04-05 21:42:02 +0100

    ga

--- main.tf
+++ main.tf
@@ -1,31 +1,9 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    snowflake = {
-      source  = "snowflake-labs/snowflake"
-      version = "0.84.1"
-    }
-  }
-}
-
 provider "snowflake" {
   alias = "security_admin"
   role  = "SECURITYADMIN"
 }
 
-
-
-resource "snowflake_database" "db" {
-  name = "TF_DEMO"
-}
-
-resource "snowflake_warehouse" "warehouse" {
-  name           = "TF_DEMO"
-  warehouse_size = "xsmall"
-  auto_suspend   = 60
-}
-
-resource "snowflake_account_role" "role" {
+resource "snowflake_role" "role" {
   provider = snowflake.security_admin
   name     = "TF_DEMO_SVC_ROLE"
 }
@@ -33,7 +11,7 @@ resource "snowflake_account_role" "role" {
 resource "snowflake_grant_privileges_to_account_role" "database_grant" {
   provider          = snowflake.security_admin
   privileges        = ["USAGE"]
-  account_role_name = snowflake_account_role.role.name
+  account_role_name = snowflake_role.role.name
   on_account_object {
     object_type = "DATABASE"
     object_name = snowflake_database.db.name
@@ -41,15 +19,15 @@ resource "snowflake_grant_privileges_to_account_role" "database_grant" {
 }
 
 resource "snowflake_schema" "schema" {
-  database            = snowflake_database.db.name
-  name                = "TF_DEMO"
+  database             = snowflake_database.db.name
+  name                 = "TF_DEMO"
   with_managed_access = false
 }
 
 resource "snowflake_grant_privileges_to_account_role" "schema_grant" {
   provider          = snowflake.security_admin
   privileges        = ["USAGE"]
-  account_role_name = snowflake_account_role.role.name
+  account_role_name = snowflake_role.role.name
   on_schema {
     schema_name = "\"${snowflake_database.db.name}\".\"${snowflake_schema.schema.name}\""
   }
@@ -58,7 +36,7 @@ resource "snowflake_grant_privileges_to_account_role" "schema_grant" {
 resource "snowflake_grant_privileges_to_account_role" "warehouse_grant" {
   provider          = snowflake.security_admin
   privileges        = ["USAGE"]
-  account_role_name = snowflake_account_role.role.name
+  account_role_name = snowflake_role.role.name
   on_account_object {
     object_type = "WAREHOUSE"
     object_name = snowflake_warehouse.warehouse.name
@@ -74,7 +52,7 @@ resource "snowflake_user" "user" {
   provider          = snowflake.security_admin
   name              = "tf_demo_user"
   default_warehouse = snowflake_warehouse.warehouse.name
-  default_role      = snowflake_account_role.role.name
+  default_role      = snowflake_role.role.name
   default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
   rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
@@ -82,7 +60,7 @@ resource "snowflake_user" "user" {
 resource "snowflake_grant_privileges_to_account_role" "user_grant" {
   provider          = snowflake.security_admin
   privileges        = ["MONITOR"]
-  account_role_name = snowflake_account_role.role.name
+  account_role_name = snowflake_role.role.name
   on_account_object {
     object_type = "USER"
     object_name = snowflake_user.user.name
@@ -91,6 +69,6 @@ resource "snowflake_grant_privileges_to_account_role" "user_grant" {
 
 resource "snowflake_grant_account_role" "grants" {
   provider  = snowflake.security_admin
-  role_name = snowflake_account_role.role.name
+  role_name = snowflake_role.role.name
   user_name = snowflake_user.user.name
 }
