97f9a48d6960d788b20270d32a95dcdf32bd3a82
Bobbili Rajashekar Reddy <bobbilirajashekarreddy@Bobbilis-MacBook-Pro.local>
2025-04-05 21:35:47 +0100

    ga

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.87"
+      source  = "snowflake-labs/snowflake"
+      version = "0.84.1"
     }
   }
 }
